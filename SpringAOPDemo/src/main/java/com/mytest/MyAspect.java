bed796e4a25de144ed72befe290ec2639dbdc49b
zhaoshichen <zhaoshichen1@gmail.com>
2021-02-15 23:29:43 +0800

    增加差异化的before和after逻辑

--- SpringAOPDemo/src/main/java/com/mytest/MyAspect.java
+++ SpringAOPDemo/src/main/java/com/mytest/MyAspect.java
@@ -13,6 +13,12 @@ public class MyAspect {
     // 所有当前包的任意类中 public的doXXX方法都执行
     @Before("execution(public * com.mytest.*.do*(..))")
     public void doLog(){
-        System.out.println("---模拟记录Log");
+        System.out.println("[Before]---模拟记录Log");
+    }
+
+    // 所有当前包的任意类中 public的任意方法都执行
+    @After("execution(public * com.mytest.*.*(..))")
+    public void doTrans(){
+        System.out.println("[After]---模拟管理事务---");
     }
 }
